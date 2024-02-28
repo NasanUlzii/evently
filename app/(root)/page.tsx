a5ecf87aa91578557a00cb566bbed4dc04240ccb
Sumber <nasa66@hotmail.co.uk>
2024-02-28 22:16:03 +0000

    third

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,17 +1,17 @@
-// import CategoryFilter from '@/components/shared/CategoryFilter';
+import CategoryFilter from '@/components/shared/CategoryFilter';
 // import Collection from '@/components/shared/Collection'
 // import Search from '@/components/shared/Search'; 
 import { Button } from '@/components/ui/button'
 // import { getAllEvents } from '@/lib/actions/event.actions';
-// import { SearchParamProps } from '@/types';
+import { SearchParamProps } from '@/types';
 import Image from 'next/image'
 import Link from 'next/link'
 
-export default async function Home()//: SearchParamProps) 
+export default async function Home({searchParams}: SearchParamProps) 
 {
-  //const page = Number(searchParams?.page) || 1;
-  //const searchText = (searchParams?.query as string) || '';
-  //const category = (searchParams?.category as string) || '';
+  const page = Number(searchParams?.page) || 1;
+  const searchText = (searchParams?.query as string) || '';
+  const category = (searchParams?.category as string) || '';
 
 //   const events = await getAllEvents({
 //     query: searchText,
@@ -48,8 +48,8 @@ export default async function Home()//: SearchParamProps)
         <h2 className="h2-bold">Trust by <br /> Thousands of Events</h2>
 
         <div className="flex w-full flex-col gap-5 md:flex-row">
-          {/* <Search />
-          <CategoryFilter /> */}
+          {/* <Search />*/}
+          <CategoryFilter /> 
         </div>
 
         {/* <Collection 
